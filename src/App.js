6b63e61daf2e35d48a7103282f8c69c113e2351a
Henrique <henrique.empresa53@outlook.com>
2021-11-04 17:53:02 -0300

    Adicionando projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.scss';
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      itensExcluidos: [],
+      listaDeCompras: [
+        {
+          id: 0,
+          imagem: 'https://m.media-amazon.com/images/I/81-Yw7YyRBL._AC_SX425_.jpg',
+          nome: 'Arroz Camil'
+        },
+        {
+          id: 1,
+          imagem: 'https://m.media-amazon.com/images/I/61eZFRX7gAL._AC_SY879_.jpg',
+          nome: 'Feijão Camil'
+        },
+        {
+          id: 2,
+          imagem: 'https://api.tendaatacado.com.br/fotos/1269_mini.jpg',
+          nome: 'Macarrão'
+        },
+      ]
+    }
+  }
+
+  render() {
+    let temp = [];
+
+    const handlerExcluirProduto = (item) => {
+      this.setState({
+        itensExcluidos: [...this.state.itensExcluidos, item],
+      })
+
+      const novoArray = this.state.listaDeCompras.filter(produto => produto.id !== item.id);
+
+      this.setState({
+        listaDeCompras: novoArray
+      });
+    }
+
+    const handlerRestaurar = () => {
+      this.state.itensExcluidos.forEach(produto => {
+
+        temp.push(produto);
+        this.setState({
+          listaDeCompras: [...this.state.listaDeCompras, ...temp]
+        })
+      })
+
+      this.setState({
+        itensExcluidos: []
+      });
+    }
+
+    return (
+      <>
+        <button onClick={() => handlerRestaurar()}>
+          Restaurar Página
+        </button>
+        <section>
+          {
+            this.state.listaDeCompras.map(produto => (
+              <div className="card">
+                <img src={produto.imagem} alt="" />
+                <h2>{produto.nome}</h2>
+                <button onClick={() => handlerExcluirProduto(produto)}>
+                  Excluir produto
+                </button>
+              </div>
+            ))
+          }
+        </section>
+      </>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/CardProduto/index.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+export default class CardProduto extends Component {
+
+    render() {
+        return(
+            <div>
+                <img src="" alt="" />
+                <h3></h3>
+                <button></button>
+            </div>
+        )
+    }
+}
